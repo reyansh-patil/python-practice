986d4799f64babc8159667c35e4c0f851c050ef7
reyansh-patil <reyanshpatil.1010@gmail.com>
2026-02-20 22:58:22 +0000

    Delete p3_string_formating.py

--- p3_string_formating.py
+++
@@ -1,6 +0,0 @@
-#https://www.teach-ict.com/v/revision/pseudocode_ocr/quiz_1/quiz.php
-#Ask the user to enter their first name and surname(s). The result should be combined into a full name and displayed along with a suitable message.
-fn = input("Please enter your first name: ")#this takes in an input (a question)
-sn = input("Please enter your surname: ")
-
-print("Hello " + fn.capitalize() + " " + sn.capitalize() + ", hope you have a good day") #this will put the two names together eg, first name = john second name = johnson,that would equal to 'john johnson'.
