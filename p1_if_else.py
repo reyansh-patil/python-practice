9b1493c198c7db076c8d93af0ef80b3d56198965
reyansh-patil <reyanshpatil.1010@gmail.com>
2026-02-20 22:43:20 +0000

    Update p1_if_else.py
    Updated happy path check first

--- p1_if_else.py
+++ p1_if_else.py
@@ -1,11 +1,12 @@
 #https://www.teach-ict.com/v/revision/pseudocode_ocr/quiz_1/quiz.php
 #Ask the user to input a student grade. If the grade is 70 or above then display, you have passed. Otherwise, it will display, sorry, you have not passed
 
-grade=int(input("Enter your student grade: ")) #This asks the user what grade they got
-# the int() on line 4 tell the computer than the input is going to be a number
-if grade<70: #This line checks if it less than 70 if it is the code will display "Sorry you have not passed"
-    print("Sorry, you have not passed")
-elif grade>=70: #Now this check if they did pass ( above or equal to 70) which will display "You have passed!"
+grade=int(input("Enter your student grade: ")) # Ask user to input the grade and convert to int
+
+if grade>=70: #Now check if they did pass ( above or equal to 70) which will display "You have passed!"
     print("You have passed!")
+elif grade<70: #This line checks if it less than 70 then it's a fail
+    print("Sorry, you have not passed")
 else: #This will display invalid input if the input is not a number
     print("Invalid grade input")
+
